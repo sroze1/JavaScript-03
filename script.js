7fb25a5f819ecc26312d6eb6ba1aecf0b5b5d297
S Roze <sherozem4@gmail.com>
2021-12-10 18:52:43 +0000

    Coding challnege completed following principles which challenge the thought process of coding. How to think like a programmer and breaking down a problem to identify possible solutions, as opposed to taking the challenge head on without any structured plan or methodology

--- script.js
+++ script.js
@@ -1,45 +1,18 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const x = 23;
+// const x = 23;
 
-const calcAge = birthYear => 2037 - birthYear;
+// const calcAge = birthYear => 2037 - birthYear;
 
-console.log(calcAge);
+// console.log(calcAge);
 
-console.log("tatti yaonda");
-
-
+// console.log("tatti yaonda");
 
 
-// 1 Understand the problem
-    // What is temp amplitutde Answer: difference
-    // between highest and lowest temp.
-// - Now to complete max and min temperatures
-// - What's a sensor error? And what to do?
-
-const temperatures = [3, -2, -6, -1, 'error', 
-9, 13, 17, 15, 14, 9, 5];
 
 
-// 2 Breaking up the problem
-// - How to ignore erros?
-// - Find max value in temp array
-// - Find min value in temp array
-// Subtract min from max (amplittude) and return it
 
-const calcTempAmplitude = function(temps) {
-
-let max = temps[0];
-    for(let i = 0; i < temps.length; i++) { 
-        const curTemp = temps[i];
-
-        if(cureTemp > max) max = curTemp;
-        if(curTemp < min) min = curTemp/
-    }                                                                                                                                                                                       
-    console.log(max);
-};
-calcTempAmplitude([3,7,4, 23]);
 
 // In the beginning before we start the array the max element will
 // Be the first element of the array. As temps[i] (i is 30)
@@ -50,6 +23,74 @@ calcTempAmplitude([3,7,4, 23]);
 
 // Math.max();
 // Math.min();
+
+
+
+const t1 = [3, -2, -6, -1, 'error', 
+9, 13, 17, 15, 14, 9, 5];
+
+
+
+
+const calcTempAmplitude = function(temps) {
+
+let min = temps[0];
+let max = temps[0];
+    for(let i = 0; i < temps.length; i++) { 
+        const curTemp = temps[i];
+        if(typeof curTemp !== 'number') continue;
+
+        if(curTemp > max) max = curTemp;
+        if(curTemp < min) min = curTemp;
+    } 
+
+    console.log(max, min);
+    return max - min;
+};
+const amplitude = calcTempAmplitude(t1);
+console.log(amplitude);
+
+
+
+// 1 Understand the problem
+    // What is temp amplitutde Answer: difference
+    // between highest and lowest temp.
+// - Now to complete max and min temperatures
+// - What's a sensor error? And what to do?
+
+// 2 Breaking up the problem
+// - How to ignore erros?
+// - Find max value in temp array
+// - Find min value in temp array
+// Subtract min from max (amplittude) and return it
+
+const t2 = 
+[3, -5, -9, 2, 'error', 10, 21, 12, 17, 14, 4, 8];
+
+
+const calcTempAmplitude2 = function (temps) {
+  let min = temps[0];
+  let max = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+
+  console.log(max, min);
+  return max - min;
+};
+
+const t3 = t1.concat(t2);
+
+const amplitude2 = calcTempAmplitude(t3);
+console.log(amplitude2);
+
+
+
+
 
 
 
